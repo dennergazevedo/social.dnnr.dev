042e483febe316dc22655b6e1eec84991f14ec27
Denner Azevedo <denner.gomes@ciahering.com.br>
2024-05-05 20:43:39 -0300

    remove theme

--- src/app/components/Header/index.tsx
+++ src/app/components/Header/index.tsx
@@ -1,38 +1,7 @@
-"use client";
-import { useCallback, useEffect, useState } from "react";
 import Link from "next/link";
 import styles from "./styles.module.scss";
-import { PiMoonStarsLight, PiSunThin } from "react-icons/pi";
-
-type Themes = 'dark' | 'light'
 
 const Header: React.FC = () => {
-  const [theme, setTheme] = useState<Themes>('dark');
-
-  const toggleTheme = useCallback(() => {
-    const body = document.getElementsByTagName("body");
-    if (body) {
-      body?.[0]?.classList?.toggle("light");
-      sessionStorage.setItem("dnr@theme", theme === 'dark' ? 'light' : 'dark');
-      if(theme === 'dark'){
-        setTheme('light');
-      }else{
-        setTheme('dark');
-      }
-    }
-  }, [theme]);
-
-  useEffect(() => {
-    const sessionTheme = sessionStorage.getItem("dnr@theme")
-    if(sessionTheme){
-      setTheme(sessionTheme as Themes);
-      console.log("sessionTheme", sessionTheme)
-      const body = document.getElementsByTagName("body");
-      if (body && sessionTheme === 'light') {
-        body?.[0]?.classList?.add("light");
-      }
-    }
-  }, [])
 
   return (
     <header className={styles.pageHeader}>
@@ -41,9 +10,6 @@ const Header: React.FC = () => {
         <span>nr</span>
         <p>.dev</p>
       </Link>
-      <div onClick={toggleTheme} className={styles.pageHeaderThemeButton}>
-        {theme === 'dark' ? <PiSunThin size={20} /> : <PiMoonStarsLight size={20} />}
-      </div>
     </header>
   );
 };
