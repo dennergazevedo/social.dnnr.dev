78e907f1db0a446d8ffd9e352738ececa6f9aae1
Denner Azevedo <denner.gomes@ciahering.com.br>
2024-05-08 23:06:53 -0300

    send message

--- src/app/components/MessageBlock/index.tsx
+++ src/app/components/MessageBlock/index.tsx
@@ -1,19 +1,31 @@
 'use client'
 import { Fragment, useState } from 'react';
+import { IoCheckmarkDoneOutline } from "react-icons/io5";
+
 import styles from './styles.module.scss'
 import MessageBlockModal from './modal';
 
 const MessageBlock: React.FC = () => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [sent, setSent] = useState<boolean>(false);
 
   const toggle = () => setIsOpen(prev => !prev);
+
+  if(sent){
+    return(
+      <section className={styles.messageBlockContainer}>
+        <IoCheckmarkDoneOutline size={16}/>
+        <p>Sua mensagem foi enviada com sucesso!</p>
+      </section>
+    )
+  }
 
   return (
     <Fragment>
       <section className={styles.messageBlockContainer} onClick={toggle}>
         <p>Envie-me uma mensagem...</p>
       </section>
-      { isOpen && <MessageBlockModal toggle={toggle}/> }
+      { isOpen && <MessageBlockModal toggle={toggle} setSent={setSent} /> }
     </Fragment>
   )
 }
--- src/app/components/MessageBlock/modal.tsx
+++ src/app/components/MessageBlock/modal.tsx
@@ -1,24 +1,58 @@
 'use client'
-import { useState } from 'react';
+import { Dispatch, SetStateAction, useCallback } from 'react';
+import * as emailjs from 'emailjs-com';
+
 import styles from './styles.module.scss'
 
 interface ModalBlockProps{
   toggle: () => void
+  setSent: Dispatch<SetStateAction<boolean>>
 }
 
-const MessageBlockModal: React.FC<ModalBlockProps> = ({toggle}: ModalBlockProps) => {
-  const [message, setMessage] = useState<string>('');
+interface MessageBlockForm{
+  message: string
+  user: string
+  status: boolean
+}
 
+const MessageBlockModal: React.FC<ModalBlockProps> = ({ toggle, setSent }: ModalBlockProps) => {
+
+  async function sendMessage(event: any){
+    event.preventDefault();
+    try{
+      await emailjs.sendForm(
+        String(process.env.NEXT_PUBLIC_MAIL_SERVICE_ID), 
+        String(process.env.NEXT_PUBLIC_MAIL_TEMPLATE_ID), 
+        event.target,
+        String(process.env.NEXT_PUBLIC_MAIL_USER_ID)
+      )
+      setSent(true)
+      toggle()
+    }catch(err){
+      console.log("[!] Erro ao enviar mensagem.", err);
+    }
+  }
+  
   return (
     <section className={styles.messageBlockModalContainer} onClick={toggle}>
-      <div className={styles.messageBlockModalBody} onClick={e => e.stopPropagation()}>
-        <textarea 
-          placeholder='Envie-me uma mensagem...'
-          value={message}
-          onChange={e => setMessage(e.target.value)}
+      <form 
+        className={styles.messageBlockModalBody} 
+        onClick={e => e.stopPropagation()}
+        onSubmit={sendMessage}
+      >
+        <div className={styles.messageBlockModalTitle}>
+          Criar mensagem
+        </div>
+        <input
+          name="user"
+          placeholder='Identifique-se'
         />
-        <button>Enviar</button>
-      </div>
+        <textarea 
+          name="message"
+          placeholder='Escreva aqui a sua mensagem...'
+        />
+        <button type="submit">Enviar</button>
+      </form>
     </section>
   )
 }
