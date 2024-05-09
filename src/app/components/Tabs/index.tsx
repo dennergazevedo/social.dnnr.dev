e645a23211bb1d860f10a3de0a051b693feb99ec
Denner Azevedo <denner.gomes@ciahering.com.br>
2024-05-09 07:35:06 -0300

    fix tab select history

--- src/app/components/Tabs/index.tsx
+++ src/app/components/Tabs/index.tsx
@@ -1,5 +1,6 @@
 'use client'
-import { Fragment, useState } from 'react';
+import { Fragment, useCallback, useEffect, useState } from 'react';
+import { useRouter } from 'next/navigation';
 
 import { SliceZone } from '@prismicio/react';
 import { components } from "@/slices";
@@ -7,31 +8,47 @@ import { components } from "@/slices";
 import styles from './styles.module.scss'
 
 const Tabs: React.FC<TabsProps> = ({ feed, experiences, content }: TabsProps) => {
+  const router = useRouter()
+
   const [currentTab, setCurrentTab] = useState<Tab>("feed");
 
   const tabList: Tab[] = ["feed", "experiências", "conteúdo"]
+
+  const selectTab = useCallback((tab: Tab) => {
+    setCurrentTab(tab)
+    router?.push(`/#${tab}`)
+  }, [router])
+
+  useEffect(() => {
+    if (typeof window !== undefined) {
+      const hash = decodeURI(window?.location?.hash).replace("#", "")
+      if(tabList.includes(hash as Tab)){
+        setCurrentTab(hash as Tab)
+      }
+    }
+  }, [])
 
   return (
     <Fragment>
       <div className={styles.tabsContainer}>
         {tabList.map(tab => (
-          <button 
-            key={tab} 
-            data-selected-tab={tab === currentTab} 
-            onClick={() => setCurrentTab(tab)}
+          <button
+            key={tab}
+            data-selected-tab={tab === currentTab}
+            onClick={() => selectTab(tab)}
             className={styles.tabButton}
           >
             {tab}
           </button>
         ))}
       </div>
-      { currentTab === 'feed' && feed &&
-        <SliceZone slices={feed} components={components} /> 
+      {currentTab === 'feed' && feed &&
+        <SliceZone slices={feed} components={components} />
       }
-      { currentTab === 'experiências' && experiences &&
-        <SliceZone slices={experiences} components={components} /> 
+      {currentTab === 'experiências' && experiences &&
+        <SliceZone slices={experiences} components={components} />
       }
-      { currentTab === 'conteúdo' && content &&
+      {currentTab === 'conteúdo' && content &&
         <section className={styles.tabContent}>
           <div className={styles.contentTitleContainer}>
             <h3 className={styles.contentTitle}>Posts</h3>
@@ -39,7 +56,7 @@ const Tabs: React.FC<TabsProps> = ({ feed, experiences, content }: TabsProps) =>
               Às vezes escrevo um pouco. Aqui estão meus posts.
             </p>
           </div>
-          <SliceZone slices={content} components={components} /> 
+          <SliceZone slices={content} components={components} />
         </section>
       }
     </Fragment>
