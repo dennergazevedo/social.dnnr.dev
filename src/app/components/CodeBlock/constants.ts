6ce3f09484e442751f0d2793ce11d9be88b28eba
Denner Azevedo <denner.gomes@ciahering.com.br>
2024-05-14 17:51:34 -0300

    send error

--- src/app/components/CodeBlock/constants.ts
+++ src/app/components/CodeBlock/constants.ts
@@ -2,7 +2,7 @@ export const codeTip = [
   `<span data-color="red">function</span> <span data-color="blue">dnnrTip</span>(errors){`,
   ` <span data-color="red" data-spacing="1">try</span>{`,
   `   <span data-color="red" data-spacing="2">const</span> <span data-color="blue">attempt</span> = <span data-color="red">new</span> <span data-color="blue">Attempt</span>(errors);`,
-  `   <span data-color="red" data-spacing="2">if</span>(attempt.error) <span data-color="red">throw new</span> <span data-color="blue">Error</span>(<span data-color="yellow">"Learn this"</span>)`,
+  `   <span data-color="red" data-spacing="2">if</span>(attempt.error) <span data-color="red">throw new</span> <span data-color="blue">Error</span>(<span data-color="yellow">"Learn this"</span>, { <span data-color="red">cause</span>: attempt.error })`,
   `   <span data-color="red" data-spacing="2">else return</span> attempt;`,
   ` <span  data-spacing="1">}</span><span data-color="red">catch</span>(newError){`,
   `   <span data-color="red" data-spacing="2">return</span> <span data-color="blue">dnnrTip</span>(errors.<span>concat</span>(newError))`,
