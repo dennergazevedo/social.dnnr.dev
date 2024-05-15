46ef8e28f6d6030825b0da2f254714ba73bdf7e4
Denner Azevedo <denner.gomes@ciahering.com.br>
2024-05-14 22:15:15 -0300

    add GA

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { PrismicPreview } from "@prismicio/next";
 import { repositoryName } from "@/prismicio";
 import { Ubuntu } from "next/font/google";
+import Script from 'next/script'
 
 import "./globals.css";
 
@@ -21,6 +22,16 @@ export default function RootLayout({
   return (
     <html lang="pt-br">
       <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-89TZSTJWB5"/>
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', 'G-89TZSTJWB5');
+        `}
+      </Script>
       <body className={ubuntu.className} style={{ paddingTop: 56 }}>
         <Header />
         {children}
