8ec94c8fe8b02d54130f8534dc1f83ce28e54dec
Denner Azevedo <me@dnnr.dev>
2025-02-01 11:08:44 -0300

    feat: add cv

--- src/app/components/Profile/Header/index.tsx
+++ src/app/components/Profile/Header/index.tsx
@@ -6,6 +6,7 @@ import profileImg from './profile.jpeg'
 
 import { FaLinkedin } from "react-icons/fa6";
 import { TbTool } from "react-icons/tb";
+import { PiUserCircleGearFill } from "react-icons/pi";
 import { Fragment, useState } from "react";
 import Stories from "../../Stories";
 
@@ -19,34 +20,44 @@ const HeaderProfile: React.FC<ProfileHeaderProps> = ({ stories }: ProfileHeaderP
     <Fragment>
       <header className={styles.profileHeader}>
         <div className={styles.profilePhoto} onClick={toggleStories}>
-          <Image 
-            src={profileImg} 
-            alt="Foto de Denner em baixo d'agua com peixes"
+          <Image
+            src={profileImg}
+            alt="Foto de Denner Azevedo em baixo d'agua com peixes"
             width={120}
             height={120}
           />
         </div>
-        <a 
-          className={styles.toolsLink} 
+        <a
+          className={styles.toolsLink}
+          href="/dnr-cv.pdf"
+          download="dnr-cv.pdf"
+          aria-label="Currículo de Denner Azevedo"
+        >
+          <PiUserCircleGearFill size={20} />
+        </a>
+        <a
+          className={styles.toolsLink}
           href="https://tools.dnnr.dev"
           target="_blank"
+          aria-label="Site de ferramentas desenvolvido por Denner Azevedo"
         >
-          <TbTool size={20}/>
+          <TbTool size={20} />
         </a>
-        <a 
-          className={styles.profileFollow} 
+        <a
+          className={styles.profileFollow}
           href="https://www.linkedin.com/in/dnnr/"
           target="_blank"
+          aria-label="Rede social LinkedIn do Denner Azevedo"
         >
-          Seguir <FaLinkedin size={20}/>
+          Seguir <FaLinkedin size={20} />
         </a>
       </header>
       {
         stories?.length &&
-        <Stories 
-          stories={stories} 
-          isOpened={storiesOpened} 
-          toggle={toggleStories} 
+        <Stories
+          stories={stories}
+          isOpened={storiesOpened}
+          toggle={toggleStories}
           close={closeStories}
         />
       }
